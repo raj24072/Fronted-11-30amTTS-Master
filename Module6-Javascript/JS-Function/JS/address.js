b6693de74bafa3fad7ad0288036e5b97bb809394
raj24072 <131134772+raj24072@users.noreply.github.com>
2023-07-24 15:11:17 +0530

    Add files via upload

---
+++ Module6-Javascript/JS-Function/JS/address.js
@@ -0,0 +1,31 @@
+
+
+
+function address1()
+{
+    var add =" Park West, 142, <br> Be Alive Media Rd,<br> PRL Colony, Bodakdev,<br> Ahmedabad, <br>Gujarat 380059"
+    document.write(add)
+    document.getElementById("addr").style="color:darkcyan; font-size:16px;"
+}
+
+
+function address2()
+{
+    var add = " Ground Floor, <br> Shri Krishna Center,<br> Mithakali Six Roads,<br> Navrangpura,<br> Ahmedabad, Gujarat 380009"
+    document.write(add)
+    document.getElementById("addr2").style="color:darkcyan; font-size:16px;"
+}
+
+function address3()
+{
+    var add = "37, (basement) <br>Kanchandeep Complex, B/S Bikanerwala,<br> Nehru Nagar,<br> Ahmedabad,<br> Gujarat 380015"
+    document.write(add)
+    document.getElementById("addr3").style="color:darkcyan; font-size:16px;"
+}
+
+function address4()
+{
+    var add = "15, near Oswal Club,<br> Sweet Home Society,<br> Shreyas Tekra, Bhudarpura,<br> Ambawadi, Ahmedabad,<br> Gujarat 380015"
+    document.write(add)
+    document.getElementById("addr4").style="color:darkcyan; font-size:16px;"
+}
